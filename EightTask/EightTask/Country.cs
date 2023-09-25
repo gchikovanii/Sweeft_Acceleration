a95e32544ba3b3224693aac04702e4d5153c3142
Giorgi chikovani <gchikovanii25@gmail.com>
2023-09-25 19:07:58 +0400

    Last_Task_Finished

---
+++ EightTask/EightTask/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightTask
+{
+    public class Country
+    {
+        public Name name { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+       
+        public double[] latlng { get; set; }
+        public double area { get; set; }
+        public double population { get; set; }
+    }
+}
---
+++ EightTask/EightTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using EightTask;
+using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
+
+
+HttpClient client = new HttpClient();
+string response = await client.GetStringAsync("https://restcountries.com/v3.1/all");
+List<Country> country = JsonConvert.DeserializeObject<List<Country>>(response);
+List<string> countries = new List<string>();
+foreach (var item in country)
+{
+    var countryName = item.name.common;
+    var path = countryName + ".txt";
+    var region = item.region;
+    var subRegion = item.subregion;
+    var population = item.population;
+    var area = item.area;
+    var latlng = item.latlng;
+    using (StreamWriter sr = new StreamWriter(path))
+    {
+        sr.WriteLine("Region:" + region);
+        sr.WriteLine("Subregion: " + subRegion);
+        sr.WriteLine("Population: " + population);
+        sr.WriteLine("Area: " + area);
+        sr.Write("latlng: ");
+        foreach (var l in latlng)
+        {
+            sr.Write(l + " ");
+        }
+    }
+}
